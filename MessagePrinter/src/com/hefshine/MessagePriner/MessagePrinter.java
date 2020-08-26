3bd6d5f14865b9b4b49091ac89014b64dcfb20e6
Ajay <ajaykumavat2012@gmail.com>
2020-08-26 11:18:36 +0530

    project

---
+++ MessagePrinter/src/com/hefshine/MessagePriner/MessagePrinter.java
@@ -0,0 +1,17 @@
+package com.hefshine.MessagePriner;
+
+public class MessagePrinter {
+	
+	void printmassage(String name)
+	{
+		System.out.println("name is:"+name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MessagePrinter m1 = new MessagePrinter();
+		m1.printmassage("ajay");
+
+	}
+
+}
